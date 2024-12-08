07fe13c71f12d794c00a083859aa449ba0c1733b
fran <fran.sipic@gmail.com>
2024-12-08 21:44:00 +0100

    commit project

---
+++ scripts/script2.js
@@ -0,0 +1,11 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Get the button element
+    var contactButton = document.querySelector('.button');
+
+    // Event listener for button clicks
+    contactButton.addEventListener('click', function () {
+        alert('Hello! Thanks for reaching out!');
+    });
+});
+
